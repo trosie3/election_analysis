7cd65375333beef9b99ca0071a12c8266e0f0a60
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-07-08 16:14:55 -0700

    3.2.10 practice code

--- python_practice.py
+++ python_practice.py
@@ -40,4 +40,36 @@ if "Arapahoe" in counties or "El Paso" in counties:
     print("Arapahoe or El Paso is in the list of counties.")
 else:
     print("Arapahoe and El Paso are not in the list of counties.")
-    
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+counties = ["Arapahoe","Denver","Jefferson"]
+for county in counties:
+    print(county)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+for county in counties_dict.keys():
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+for key, value in counties_dict.items():
+    print(f"{key} county has {value} registered voters")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+
