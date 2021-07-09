8b1e1c280e84aa1b63d62d9154aed06851cfff6c
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-07-08 19:00:53 -0700

    calc total votes

--- PyPoll.py
+++ PyPoll.py
@@ -4,6 +4,12 @@ import csv
 import os
 # assign variable for the file to load and the path
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#initialize total vote 
+total_votes = 0
+
 # open election results and read
 with open(file_to_load) as election_data:
 
@@ -11,16 +17,18 @@ with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # read header row.
     headers = next(file_reader)
-    print(headers)
+ 
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # add to total vote count
+        total_votes += 1
+#print total votes        
+print(total_votes)
     
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
@@ -30,9 +38,11 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write("\n")
     txt_file.write("Arapahoe\nDenver\nJefferson")
 
-
 # 1.Total number of votes cast
+# maybe for later? ---    txt_file.write("Total number of votes str(total_votes)")
 # 2.A complete list of candidates who received votes
 # 3.Total number of votes each candidate received
 # 4.Percentage of votes each candidate won
 # 5.The winner of the election based on popular vote
+# The data we need to retrieve
+#add dependencies
