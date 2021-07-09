0ab2908e81847369c88a9ebbaee54136a5f7954f
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-07-08 18:21:01 -0700

    create txt and add first line of text to txt file

--- PyPoll.py
+++ PyPoll.py
@@ -9,6 +9,18 @@ with open(file_to_load) as election_data:
     # to do: perform analysis
     print(election_data)
 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
+
+
 # 1.Total number of votes cast
 # 2.A complete list of candidates who received votes
 # 3.Total number of votes each candidate received
