5f1732b659bc499fb36628a9d49c3b7791316905
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-07-08 18:46:45 -0700

    commit code prompt 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -10,9 +10,14 @@ with open(file_to_load) as election_data:
     # to do: perform analysis
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
     # Print each row in the CSV file.
     for row in file_reader:
         print(row)
+    
 
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
