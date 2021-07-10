95dfbcdd3ec35996abbbed4ab359bcf1ef373b73
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-07-10 10:48:09 -0700

    fixed bug, winner found

--- PyPoll.py
+++ PyPoll.py
@@ -56,17 +56,18 @@ with open(file_to_load) as election_data:
             
         #to do print each cand name, vote, and perct of #of votes
         print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+
         #deterimine winning vote count & candidate          
         if (votes > winning_count) and (vote_percentage > winning_percentage):
-            #if true then set winning count equal to votes, winning perc equal to vote perc
+        #if true then set winning count equal to votes, winning perc equal to vote perc
             winning_count = votes
             winning_percentage = vote_percentage
             #set winning cand equal to cand name
-            winning_count = candidate_name
+            winning_candidate= candidate_name
 
     #to do print winning cand, vote count & perct       
     winning_candidate_summary = (
-        f"-------------------------\n"
+        f"------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
