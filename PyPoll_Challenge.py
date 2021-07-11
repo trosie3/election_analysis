180432f1378bf9ab584b4529a4ed0cfb0aba8439
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-07-11 08:48:40 -0700

    for loop for county votes and percentage

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -91,16 +91,18 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-
+    for county_name in county_options:
         # 6b: Retrieve the county vote count.
-
+        vote_turnout = county_votes[county_name]
         # 6c: Calculate the percentage of votes for the county.
-
+        turnout_percentage = float(vote_turnout)/float(total_votes)*100
 
          # 6d: Print the county results to the terminal.
-
+        county_results = (
+            f'{county_name}: {turnout_percentage:.1f}% ({vote_turnout:,})\n')
+        print(county_results)
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
 
 
